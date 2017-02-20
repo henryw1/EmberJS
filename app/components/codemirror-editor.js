3680255338557cc3586bad13f306cc532afb750e
Osvaldo Sousa <Osvaldo Sousa>
2017-02-20 15:13:35 +0000

    extra changes

--- app/components/codemirror-editor.js
+++ app/components/codemirror-editor.js
@@ -3,9 +3,9 @@ var inject =  Ember.inject;
 export default Ember.Component.extend({
 classNames: ['myEditor'],
 
-var cdd = CodeMirror(ocument.body, {
-  value: "function myScript(){return 100;}\n",
-  mode:  "javascript"
-});
-
+// var cdd = CodeMirror(document.body, {
+//   value: "function myScript(){return 100;}\n",
+//   mode:  "javascript"
+// });
+//
 });
